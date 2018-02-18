e01de7c2dc908405a68695bad45c7535663a6166
Jostein Kjønigsen <jostein@kjonigsen.net>
2018-02-18 13:39:31 +0100

    More informative commentary.

--- bmx-mode.el
+++ bmx-mode.el
@@ -25,8 +25,11 @@
 
 ;;; Commentary:
 
-;; This minor-mode is cross-platform and does not depend on any native
-;; platform tools.
+;; Extend Emacs' bat-mode to support auto-completion, code-navigation and
+;; refactoring capabilities.
+;;
+;; This minor-mode is all Elisp, and thus cross-platform and does not depend
+;; on any native platform tools or host OS.
 
 ;;; Code:
 
